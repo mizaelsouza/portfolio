e621db775be420396300e272d928afa2ece19aa9
mizael <mizaelsouza31@gmail.com>
2022-04-04 10:05:51 -0300

    Enviado para deploy

--- knexfile.js
+++ knexfile.js
@@ -6,9 +6,12 @@ module.exports = {
         version: "8.0.26",
         connection: {
             database: "web",
-            host: "aws-saci.c438zmpqmy0o.sa-east-1.rds.amazonaws.com",
+           /* host: "aws-saci.c438zmpqmy0o.sa-east-1.rds.amazonaws.com",
             user: "admin",
-            password: "M649235M",
+            password: "M649235M",*/
+            host: env.process.HOST_AWS,
+            user: env.process.USER_AWS,
+            password: env.process.PASS,
         },
     },
     pool: { min: 0, max: 7 },
